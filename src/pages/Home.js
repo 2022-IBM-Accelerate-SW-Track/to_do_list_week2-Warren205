3534264f045272e94b433b8462a9330c530de66a
Warren Spencer <warrenspencer41@gmail.com>
2022-06-19 19:24:10 -0500

    feat: IBM Accelerate

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -32,7 +32,7 @@ class Home extends Component {
     // Create a array that contains the current array and the new todo item
     let new_list = [...this.state.todos, todo];
     todo.id = Math.random();
-    if (this.state.todos.find(t => t.content.trim().toLowerCase() === todo.content.trim())){
+    if (this.state.todos.find(t => t.content.trim().toLowerCase() === todo.content.trim().toLowerCase())){
       return;
     }
     else{
@@ -50,7 +50,6 @@ class Home extends Component {
         {/* When passing the AddTodo component, addTodo is a prop that is used in the 
         AddTodo.js file when handling the submit */}
         <AddTodo addTodo={this.addTodo} />
-        <deleteTodo deleteTodo={this.deleteTodo} />
         {/* When returning the Todos component, todos is a prop passed to the todos.js file
          to format and render the current todo list state */}
         <Todos todos={this.state.todos} deleteTodo={this.deleteTodo} />
